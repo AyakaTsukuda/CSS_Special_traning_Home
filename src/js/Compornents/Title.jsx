a41257f05a91cabc01cef25a83e6e3552fdfbe95
AyakaTsukuda <37703284+AyakaTsukuda@users.noreply.github.com>
2023-06-03 01:11:28 +0900

    work in progress

--- src/js/Compornents/Title.jsx
+++ src/js/Compornents/Title.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Title() {
+export default function Title({text="Title"}) {
 
     return (
-        <div className="">
-            
+        <div className="mb-2.5 text-lg font-bold" style={{fontFamily:"Meiryo", color:"#333333"}}>
+            {text}
         </div>
     )
 }
--- src/js/Pages/Photo.jsx
+++ src/js/Pages/Photo.jsx
@@ -1,14 +1,21 @@
-
+import TopImage from "../../Images/photo_book_top.jpg"
+import Container from "../Compornents/Container";
+import Title from "../Compornents/Title";
 
 
 export default function Photo() {
 
     return (
-        <div>
-            <header>
-                <h1>PHOTO BOOK</h1>
-                {/* @todo: 明日この続きから。。。 */}
-            </header>
+        <div className="absolute h-screen w-screen" style={{backgroundColor:"#F4F9FF"}}>
+            <div style={{"margin":"60px 140px"}}>
+                <header className="flex flex-col gap-3.5">
+                    <h1 className="text-xl font-bold" style={{fontFamily:"Meiryo", color:"#333333"}}>PHOTO BOOK</h1>
+                    <img className="object-cover" style={{height:"400px"}} src={TopImage} alt="top-img" />
+                </header>
+                <Container addClassName="bg-white" addStyle={{padding:"30px"}}>
+                    <Title text="INDEX" />
+                </Container>
+            </div>
         </div>
     );
 }
