7a9d7bcb742ec1e2451d05980aec45ad167f8ebd
AyakaTsukuda <37703284+AyakaTsukuda@users.noreply.github.com>
2023-06-02 23:42:35 +0900

    tailwindcss setting

---
+++ src/js/Pages/Photo.jsx
@@ -0,0 +1,16 @@
+
+
+
+export default function Photo() {
+
+    return (
+        <div>
+            <header>
+                <h1>PHOTO BOOK</h1>
+                {/* @todo: 明日この続きから。。。 */}
+            </header>
+        </div>
+    );
+}
+
+
